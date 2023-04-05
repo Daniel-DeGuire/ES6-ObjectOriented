067f3a0568dce3630e24f0339ee92a2a20eba5fa
Daniel DeGuire <danjdeguire@gmail.com>
2023-04-04 21:17:09 -0500

    add code for add methods

---
+++ add-methods-after-inheritance.js
@@ -0,0 +1,21 @@
+function Animal() {}
+Animal.prototype.eat = function () {
+  console.log("nom nom nom");
+};
+
+function Dog() {}
+
+Dog.prototype = Object.create(Animal.prototype);
+Dog.prototype.constructor = Dog;
+
+Dog.prototype.bark = function () {
+  console.log("Woof!");
+};
+
+Dog.protoype = object.create(Animal.prototype);
+
+Dog.prototype.constructor = Dog;
+
+Dog.prototype.bark = function () {
+  console.log("Woof!");
+};
